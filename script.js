f7b8606f7832f70acfad7a79774bb3c9a16a919a
sujeong165 park <sujr110026@gmail.com>
2025-03-23 05:46:07 +0000

    03.23_연습

---
+++ script.js
@@ -0,0 +1,13 @@
+document.addEventListener("DOMContentLoaded", function () {
+    // 포리자판기 검색 기능
+    const searchButton = document.querySelector(".vending-machine button");
+    searchButton.addEventListener("click", function () {
+        alert("가까운 포리자판기 검색 기능은 준비 중입니다!");
+    });
+
+    // 성 지식 퀴즈 정답 버튼
+    const quizButton = document.querySelector(".quiz button");
+    quizButton.addEventListener("click", function () {
+        alert("정답: 다양한 크기가 존재해요!");
+    });
+});
